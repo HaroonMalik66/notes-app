fa6d1edbb7642d3c98cce923027ca7edff33a129
Harooon Ahmed <haroonmalik4219855@gmail.com>
2022-10-31 10:11:54 +0500

    solve key problem in useLocalStorageHook

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -1,20 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function useLocalStorageState(key, defaultVal) {
-    // make piece of state based of value in local storage or defaultVal
-    const [state, setState] = useState(() => {
-        var val;
-        try {
-            val = JSON.parse(window.localStorage.getItem(key) || String(defaultVal));
-        } catch (e) {
-            val = defaultVal;
-        }
-        return val;
-    })
-    // useEffect to update local storage
-    useEffect(() => {
-        window.localStorage.setItem(key, JSON.stringify(state))
-    }, [state])
-    return [state, setState]
+  // make piece of state based of value in local storage or defaultVal
+  const [state, setState] = useState(() => {
+    var val;
+    try {
+      val = JSON.parse(window.localStorage.getItem(key) || String(defaultVal));
+    } catch (e) {
+      val = defaultVal;
+    }
+    return val;
+  });
+  // useEffect to update local storage
+  useEffect(
+    (key) => {
+      window.localStorage.setItem(key, JSON.stringify(state));
+    },
+    [state]
+  );
+  return [state, setState];
 }
 export default useLocalStorageState;
